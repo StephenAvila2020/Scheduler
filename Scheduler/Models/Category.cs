3111172e959bbe7eb84df93255d38f48e2645004
StephenAvila2020 <stephen.r.avila@gmail.com>
2020-10-16 15:59:36 -0400

    added

---
+++ Scheduler/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public string Name { get; set; }
+        public DateTime StartOne { get; set; }
+        public DateTime EndOne { get; set; }
+        public DateTime StartTwo { get; set; }
+        public DateTime EndTwo { get; set; }
+    }
+}
+
